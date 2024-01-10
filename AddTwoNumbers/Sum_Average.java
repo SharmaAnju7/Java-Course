f6a36e180bf6b2640b6a4fd35950e2b8174910c6
SharmaAnju7 <121777528+SharmaAnju7@users.noreply.github.com>
2024-01-10 11:46:08 +0545

    Sum_Averages

---
+++ AddTwoNumbers/Sum_Average.java
@@ -0,0 +1,22 @@
+//Sum_Average.java
+import java.util.Scanner;
+public class Sum_Average{
+    public static void main(String[]args){
+        int n, sum=0;
+        float average;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter no. of Elements You want To Array: ");
+
+        n = s. nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter all the Elements: ");
+        for(int i=0; i<n; i++)
+        {
+            a[i] = s.nextInt();
+            sum = sum+a[i];
+        }
+        System.out.println("Sum: "+sum);
+        average = (float)sum/n;
+        System.out.println("Average:"+average);
+    }
+}
